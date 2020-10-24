ae17953cd046191c1f58ae492f9ec30ea9f7dfc4
Thomas Leroy <thomasleroy50@gmail.com>
2020-10-24 10:46:48 +0200

    first commit

---
+++ Bicycle.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'Vehicle.php';
+
+class Bicycle extends Vehicle
+{
+  public function __construct(string $color, int $nbSeats)
+  {
+      parent::__construct($color, $nbSeats);
+  }
+}
